76d0213cf015dba2d58df3e872042eb4eea0ca76
georg <georgkaasik11@gmail.com>
2025-10-16 16:36:56 +0300

    Has a bug where reprints first 3 lines excluding the rest

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,37 @@ use std::{thread, time::Duration};
 use std::io::{self, Write};
 use sysinfo::{System, SystemExt};
 use crossterm::{execute, terminal, cursor};
+use colored::Colorize;
 
 fn main() -> io::Result<()> {
     let mut sys = System::new_all();
     let mut stdout = io::stdout();
-    loop {
-        sys.refresh_all();
 
+    loop {
         execute!(
             stdout,
             terminal::Clear(terminal::ClearType::All), // clear screen
-            cursor::MoveTo(0, 0),                     // move cursor to top-left
+            cursor::MoveTo(0, 0), // move cursor to top-left
         )?;
 
-        println!("System monitor");
-        println!("==================================================================");
+        writeln!(stdout, "{}", "System monitor".yellow())?;
+        writeln!(stdout, "press Ctrl+C to quit")?;
+        writeln!(stdout, "==================================================================")?;
+        sys.refresh_all();
         println!("OS name: {:?}", get_os_name(&mut sys));
         println!("OS version: {:?}", get_os_version(&mut sys));
         println!("Kernel version: {:?}", get_kernel_version(&mut sys));
-        println!("Host: {:?}", get_hostname(&mut sys));
-        println!("\nCPU usage: {:.0}%     Memory: ({:.2} GB / {:.2} GB)", get_cpu_usage(&mut sys),
-                                                                        get_used_memory(&mut sys),
-                                                                        get_total_memory(&mut sys));
+        println!("Host: {:?}\n", get_hostname(&mut sys));
+        print_disk_info(&mut sys);
+        println!("\nMemory(RAM): {} / {}", get_used_memory(&mut sys).yellow(), get_total_memory(&mut sys).red());
+        println!("{}: {:?} - {:.2} MHz - {} usage\n",  "CPU".yellow(),
+                 get_cpu_brand(&mut sys),
+                 get_cpu_frequency(&mut sys),
+                 get_cpu_usage(&mut sys).yellow());
+        println!("CPU cores:");
+        print_cpu_cores_info(&mut sys);
 
+        stdout.flush()?;
         thread::sleep(Duration::from_secs(1));
     }
 }
